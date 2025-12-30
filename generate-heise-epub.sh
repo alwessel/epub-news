eb9f7c9179a22b45882f9fab5d206c581a8d6b3e
Alexander Wessel <alexander.wessel@pentacor.de>
2025-12-30 14:11:52 +0100

    improve font size and margin

--- generate-heise-epub.sh
+++ generate-heise-epub.sh
@@ -4,6 +4,25 @@ set -e
 outputFile="heise-$(date '+%d.%m.%Y').pdf"
 rm -f $outputFile
 
-ebook-convert heise.recipe $outputFile -v
+# style recommendations from https://remarkable.jms1.info/scripts/rm2-make-pdf.html
+
+ebook-convert heise.recipe "$outputFile" -v \
+    --output-profile generic_eink_hd \
+    --base-font-size 14 \
+    --embed-all-fonts               \
+    --subset-embedded-fonts         \
+    --unsmarten-punctuation         \
+    --custom-size 1404x1872 \
+    --unit devicepixel \
+    --pdf-sans-family 'Noto Sans' \
+    --pdf-serif-family 'Noto Serif' \
+    --pdf-mono-family 'Noto Mono' \
+    --pdf-standard-font sans \
+    --pdf-mono-font-size 14 \
+    --pdf-page-margin-left 10 \
+    --pdf-page-margin-right 10 \
+    --pdf-page-margin-top 5 \
+    --pdf-page-margin-bottom 5 \
+    --preserve-cover-aspect-ratio
 
 echo "Successfully generated: $outputFile"
